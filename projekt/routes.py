75d9fbf4f64fbeb5b094de90612c8ad6c48ede3a
Jonas A. <jonas.arnold@alumni.fh-aachen.de>
2025-11-16 12:39:44 +0100

    added product site

--- projekt/routes.py
+++ projekt/routes.py
@@ -199,7 +199,21 @@ def index():
     products = Product.query.all()
     return render_template('index.html', products=products, cart=get_cart())
 
-# --- product_new AND product_delete WERE REMOVED AS REQUESTED ---
+# +++ NEUE ROUTE FÜR PRODUKTDETAILS +++
+@app.route('/product/<string:product_id>')
+def product_detail(product_id):
+    """
+    Zeigt die Detailseite für ein einzelnes Produkt an.
+    """
+    # 1. Lokale Produktdaten abrufen (Name, Preis, Beschreibung)
+    product = Product.query.get_or_404(product_id)
+    
+    # 2. ECHTZEIT-RPC: Lagerbestand aus dem ERP abrufen
+    real_stock = get_erp_stock(product.id)
+    
+    # 3. Neue Template-Datei rendern und Daten übergeben
+    return render_template('product_detail.html', product=product, stock=real_stock)
+# +++ ENDE NEUE ROUTE +++
 
 
 # --- Auth & User Routes ---
