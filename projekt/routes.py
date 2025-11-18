0792c76962bebd0c31740593b2fcd25dc5211a00
yhfrds <yudhafirdaus736@gmail.com>
2025-11-18 20:11:24 +0100

    successfully separating the products routes

--- projekt/routes.py
+++ projekt/routes.py
@@ -193,29 +193,6 @@ def clear_cart():
     session.pop('cart', None)
     session.modified = True
 
-# --- General & Product Routes ---
-@app.route('/')
-def index():
-    products = Product.query.all()
-    return render_template('index.html', products=products, cart=get_cart())
-
-# +++ NEUE ROUTE FÜR PRODUKTDETAILS +++
-@app.route('/product/<string:product_id>')
-def product_detail(product_id):
-    """
-    Zeigt die Detailseite für ein einzelnes Produkt an.
-    """
-    # 1. Lokale Produktdaten abrufen (Name, Preis, Beschreibung)
-    product = Product.query.get_or_404(product_id)
-    
-    # 2. ECHTZEIT-RPC: Lagerbestand aus dem ERP abrufen
-    real_stock = get_erp_stock(product.id)
-    
-    # 3. Neue Template-Datei rendern und Daten übergeben
-    return render_template('product_detail.html', product=product, stock=real_stock)
-# +++ ENDE NEUE ROUTE +++
-
-
 # --- Auth & User Routes ---
 @app.route('/register', methods=['GET', 'POST'])
 def register():
